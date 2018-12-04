7dcba1aae9d28431e9e9a8c88c9d838cfc3780f2
Stinnia <Stinnia@users.noreply.github.com>
2018-12-04 13:14:34 +0100

    app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
