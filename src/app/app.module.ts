ec812458c21ae3bbd1c36829c320debe4492c59a
okomaro <okomaro@gmail.com>
2018-12-04 13:12:35 +0100

    new  component place

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { PlaceComponent } from './place/place.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    PlaceComponent
   ],
   imports: [
     BrowserModule,
